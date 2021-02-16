193f94edfa1f89c5a2a48cb8cd4bdde58e0ea351
Ivan da Silva Rosa <iivan.si@msn.com>
2021-02-16 20:43:44 -0300

    estudo antes de remover tudo para aplicar template

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const router = express.Router();
 // rota página inicial
 router.get('/', (request, response) => {
 
-    let nome = request.query.nome
-    let sobrenome = request.query.sobrenome
+    /*let nome = request.query.nome;
+    let sobrenome = request.query.sobrenome;
 
     response.send(`
     <div style="
@@ -27,26 +27,43 @@ router.get('/', (request, response) => {
             Página Inicial
         </h1>
 
-        <h2>
+        <p>
             ${nome}
-            <br />
-            ${sobrenome}
-        </h2>
+        </p>
 
-        <h3>
-            <a href="/sobre" style="
-                text-decoration: none;
-                background-color: #157347;
-                padding: 20px 40px;
-                border-radius: 5px;
-                color: #fff;
-                font-family: Helvetica;
-            ">
-                Ir para a página sobre
-            </a>
-        </h3>
+        <a href="/sobre" style="
+            display: inline-block;
+            text-decoration: none;
+            background-color: #157347;
+            padding: 20px 40px;
+            border-radius: 5px;
+            color: #fff;
+            font-family: Helvetica;
+        ">
+            Ir para a página sobre
+        </a>
+
+        <a href="/posts/111" style="
+            display: inline-block;
+            text-decoration: none;
+            background-color: #424242;
+            padding: 20px 40px;
+            border-radius: 5px;
+            color: #fff;
+            font-family: Helvetica;
+        ">
+            Ler Post
+        </a>
     </div>
-    `);
+    `);*/
+
+    /*
+    GET: request.query;
+    POST: request.body;
+    URL_PARAMS: request.params
+    */
+   
+    response.json(request.query);
     
 });
 
@@ -70,20 +87,55 @@ router.get('/sobre', (request, response) => {
             Página sobre
         </h1>
 
-        <h3>
-            <a href="/?nome=Ivan&sobrenome=da Silva Rosa" style="
-                text-decoration: none;
-                background-color: #0b5ed7;
-                padding: 20px 40px;
-                border-radius: 5px;
-                color: #fff;
-                font-family: Helvetica;
-            ">
-                Ir para a página Inicial
-            </a>
-        </h3>
+        <a href="/?nome=Ivan&sobrenome=dSR" style="
+            isplay: inline-block;
+            text-decoration: none;
+            background-color: #0b5ed7;
+            padding: 20px 40px;
+            border-radius: 5px;
+            color: #fff;
+            font-family: Helvetica;
+        ">
+            Ir para a página Inicial
+        </a>
     </div>
     `);
+});
+
+router.get('/posts/:id', (request, response) => {
+    let id = request.params.id;
+    response.send(`
+    <div style="
+        padding: 20px 50px;
+        box-sizing: border-box;
+        text-align: center;
+    ">
+        <h1 style="
+            background-color: #157347;
+            padding: 20px;
+            font-family: Helvetica;
+            text-transform: uppercase;
+            border-radius: 5px;
+            margin-bottom: 100px;
+            color: #fff;
+        ">
+            Leitura do post ${id}
+        </h1>
+
+        <a href="/?nome=Ivan&sobrenome=dSR" style="
+            isplay: inline-block;
+            text-decoration: none;
+            background-color: #0b5ed7;
+            padding: 20px 40px;
+            border-radius: 5px;
+            color: #fff;
+            font-family: Helvetica;
+        ">
+            Ir para a página Inicial
+        </a>
+    </div>
+    `);
+    
 });
 
 module.exports = router;
