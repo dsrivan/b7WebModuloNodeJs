1781d5d844063a44b4db6b8e43ee4bd98976f1d9
Ivan da Silva Rosa <iivan.si@msn.com>
2021-02-17 20:08:40 -0300

    flash messages e try/catch

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require('express');
 // Mustache (template engine)
 const mustache = require('mustache-express');
 
+// cookie (mensagens flash)
+const cookieParser = require('cookie-parser');
+
+// sessão (mensagens flash)
+const session = require('express-session');
+
+// sessão (mensagens flash)
+const flash = require('express-flash');
+
 // Rotas
 const router = require('./routes');
 
@@ -14,18 +23,33 @@ const errorHandler = require('./handlers/errorHandler');
 // configurações
 const app = express();
 
-// helpers (middleware global)
-app.use((request, response, next) => {
-    //variáveis globais
-    response.locals.h = helpers;
-    next();
-});
-
 // para usar em formato json (middleware global)
 app.use(express.json());
 
 //
 app.use(express.urlencoded({extended:true}));
+
+// habilita o cookie
+app.use(cookieParser(process.env.SECRET));
+
+// habilita o session
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false, // não precisa destruir e recriada caso nada ter sido alterado
+    saveUninitialized: false // não salva a sessão se ela for iniciada sem nenhum dado
+}));
+
+// habilita o cookie
+app.use(flash());
+
+// helpers (middleware global)
+app.use((request, response, next) => {
+    //variáveis globais
+    response.locals.h = helpers;
+    // flashes disponíveis globalmente
+    response.locals.flashes = request.flash();
+    next();
+});
 
 // rota inicial (middleware global)
 app.use('/', router);
--- controllers/postController.js
+++ controllers/postController.js
@@ -13,8 +13,16 @@ exports.addAction = async (request, response) => {
     // cria o tipo de post com base no model Post
     const post = new Post(request.body);
 
-    // grava o post no banco
-    await post.save();
+    try {
+        // grava o post no banco
+        await post.save();
+    } catch(error) {
+        request.flash('error', 'Erro: ' + error.message);
+        return request.redirect('/post/add');
+        /* o return é para parar a execução do código */
+    }
+    // exibe mensagem flash
+    request.flash('success', 'Post salvo com sucesso!');
 
     // redireciona para a home
     response.redirect('/');
